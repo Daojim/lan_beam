84d755409b74c24d597d2a08bfb0c489f1df90a3
Daojim <jimmyddao@gmail.com>
2025-06-29 02:34:32 -0400

    Create AppState object at top level and any screen can now watch/read AppState

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'models/app_state.dart';
+import 'models/app_settings.dart';
 
 void main() {
-  runApp(const LanBeamApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AppState(
+        discoveredDevices: [],
+        selectedFile: null,
+        activeTransfer: null,
+        settings: AppSettings(
+          localDeviceName: 'MyDevice',
+          defaultSaveFolder: 'C:/Users/Jimmy/Downloads',
+        ),
+        isListening: false,
+      ),
+      child: const LanBeamApp(),
+    ),
+  );
 }
 
 class LanBeamApp extends StatelessWidget {
